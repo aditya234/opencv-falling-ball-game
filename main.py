c042e29cde18aa9fda82da410cfbee5003cbdc21
Aditya Soni <adityasoni5859@gmail.com>
2021-08-22 23:46:29 +0530

    added background extraction logic

--- main.py
+++ main.py
@@ -1,1 +1,71 @@
 import cv2
+import numpy as np
+from collections import deque
+
+
+class BackgroundExtraction:
+    def __init__(self, width, height, scale, maxlen=10):
+        self.maxlen = maxlen
+        self.scale = scale
+        self.width = width // scale
+        self.height = height // scale
+        self.buffer = deque(maxlen=maxlen)
+        self.background = None
+
+    def calculate_background(self):
+        self.background = np.zeros((self.height, self.width), dtype='float32')
+        for item in self.buffer:
+            self.background += item
+        self.background /= len(self.buffer)
+
+    def update_background(self, old_frame, new_frame):
+        self.background -= old_frame / self.maxlen
+        self.background += new_frame / self.maxlen
+
+    def update_frame(self, frame):
+        if len(self.buffer) < self.maxlen:
+            self.buffer.append(frame)
+            self.calculate_background()
+        else:
+            old_frame = self.buffer.popleft()
+            self.buffer.append(frame)
+            self.update_background(old_frame, frame)
+
+    def get_background(self):
+        return self.background.astype('uint8')
+
+    def apply(self, frame):
+        down_scale = cv2.resize(frame, (self.width, self.height))
+        gray = cv2.cvtColor(down_scale, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        self.update_frame(gray)
+        abs_diff = cv2.absdiff(bg_buffer.get_background(), gray)
+        _, ad_mask = cv2.threshold(abs_diff, 15, 255, cv2.THRESH_BINARY)
+        return ad_mask
+
+
+width = 640
+height = 480
+scale = 2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+bg_buffer = BackgroundExtraction(width, height, scale, maxlen=5)
+
+while True:
+    # Reading, resizing, and flipping the frame
+    _, frame = cap.read()
+    frame = cv2.resize(frame, (width, height))
+    frame = cv2.flip(frame, 1)
+
+    # Processing the frame
+    fg_mask = bg_buffer.apply(frame)
+
+    cv2.imshow("FG Mask", fg_mask)
+    cv2.imshow("Webcam", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
